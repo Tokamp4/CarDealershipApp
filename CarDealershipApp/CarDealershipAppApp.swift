ce661e8e7ee063c474e2faa3e7247eca11ad3ac3
Eduardo Tokarski <eduardo.tokarski.almeida@gmail.com>
2025-03-11 09:11:22 -0400

    Initial Commit

---
+++ CarDealershipApp/CarDealershipAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarDealershipAppApp.swift
+//  CarDealershipApp
+//
+//  Created by Henrique on 2025-03-11.
+//
+
+import SwiftUI
+
+@main
+struct CarDealershipAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
