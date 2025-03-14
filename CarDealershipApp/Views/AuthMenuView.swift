63dc787a664e15965901692f300bec941133cba9
Tokamp4 <eduardo.tokarski.almeida@gmail.com>
2025-03-14 18:24:05 -0400

    finalized authmenuview

--- CarDealershipApp/Views/AuthMenuView.swift
+++ CarDealershipApp/Views/AuthMenuView.swift
@@ -10,22 +10,20 @@ import SwiftUI
 struct AuthMenuView: View {
     var body: some View {
         VStack{
-            Image("bgAuth")
-                .resizable()
-                .edgesIgnoringSafeArea(.top)
-                .frame(maxWidth: .infinity, maxHeight: 150) // Corrected .frame for dynamic width
-                .shadow(color: .gray, radius: 5, x: 5, y: 5)
-                .overlay(
-                    Text("Get Started")
-                        .font(.system(size: 40, weight: .bold, design: .default))
-                        .foregroundColor(.black)
-                        .frame(width: 140)
-                        .padding(.top, 40)
-                        .padding(.leading, 40),
-                    alignment: .bottomLeading // Correct alignment syntax
-                )
-
+            ZStack{
+                Image("bgAuth")
+                    .resizable()
                     
+                    .frame(maxWidth: .infinity, maxHeight: 200) // Corrected .frame for dynamic width
+                    .edgesIgnoringSafeArea(.top)
+                    .shadow(color: .gray, radius: 5, x: 5, y: 5)
+                Text("Get Started")
+                    .font(.system(size: 60, weight: .bold, design: .default))
+                    .foregroundColor(.black)
+                    .frame(width: 210)
+                    .padding(.top, 140)
+                    .padding(.trailing, 80)
+            }.edgesIgnoringSafeArea(.top)
             Spacer()
          
             Text("Don't have an account yet?")
