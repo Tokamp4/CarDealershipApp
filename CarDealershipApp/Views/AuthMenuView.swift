0b36b602dc68f42df879962b026c18cf2e5ad22c
Tokamp4 <eduardo.tokarski.almeida@gmail.com>
2025-03-13 20:35:44 -0400

    roughly finished authmenuview

--- CarDealershipApp/Views/AuthMenuView.swift
+++ CarDealershipApp/Views/AuthMenuView.swift
@@ -13,19 +13,55 @@ struct AuthMenuView: View {
             Image("bgAuth")
                 .resizable()
                 .edgesIgnoringSafeArea(.top)
-                .frame(width: .infinity, height: 150)
+                .frame(maxWidth: .infinity, maxHeight: 150) // Corrected .frame for dynamic width
                 .shadow(color: .gray, radius: 5, x: 5, y: 5)
-                .overlay{
-                    
-                }
-                    
+                .overlay(
+                    Text("Get Started")
+                        .font(.system(size: 40, weight: .bold, design: .default))
+                        .foregroundColor(.black)
+                        .frame(width: 140)
+                        .padding(.top, 40)
+                        .padding(.leading, 40),
+                    alignment: .bottomLeading // Correct alignment syntax
+                )
+
                     
             Spacer()
-            Button{
+         
+            Text("Don't have an account yet?")
+                .font(.system(size: 25, weight: .regular, design: .default))
                 
+            
+            Button{
+                //sign up function
             } label: {
-                Text("Login")
+                RoundedRectangle(cornerRadius: 15)
+                    .foregroundColor(.black)
+                    .frame(width: 300, height: 70)
+                    .overlay(
+                        HStack (alignment: .center){
+                            Text("Sign Up")
+                                .font(.title)
+                                .foregroundColor(.white)
+                        }.padding()
+                    )
+            }.padding(.bottom, 50)
+            
+            Button{
+                //sign up function
+            } label: {
+                RoundedRectangle(cornerRadius: 15)
+                    .foregroundColor(.black)
+                    .frame(width: 300, height: 70)
+                    .overlay(
+                        HStack (alignment: .center){
+                            Text("Login")
+                                .font(.title)
+                                .foregroundColor(.white)
+                        }.padding()
+                    )
             }
+            Spacer()
         }
         
     }
