9ecb5bf37fc1ca4f5d41ab7aef8ca968524a2752
Tokamp4 <eduardo.tokarski.almeida@gmail.com>
2025-03-19 11:47:58 -0400

    added conversation header view

---
+++ CarDealershipApp/Views/Components/ChatHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatHeaderView.swift
+//  CarDealershipApp
+//
+//  Created by Henrique on 2025-03-19.
+//
+
+import SwiftUI
+
+struct ChatHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatHeaderView()
+}
