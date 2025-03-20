ae1ea4a8048bc2d72d92cc805161a775cfae2569
Tokamp4 <eduardo.tokarski.almeida@gmail.com>
2025-03-20 15:11:19 -0400

    organized app navigation for presentation

---
+++ CarDealershipApp/Views/Components/ImageCarousel.swift
@@ -0,0 +1,18 @@
+//
+//  ImageCarousel.swift
+//  CarDealershipApp
+//
+//  Created by Henrique on 2025-03-20.
+//
+
+import SwiftUI
+
+struct ImageCarousel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageCarousel()
+}
---
+++ CarDealershipApp/Views/MainContainerView.swift
@@ -0,0 +1,18 @@
+//
+//  MainContainerView.swift
+//  CarDealershipApp
+//
+//  Created by Henrique on 2025-03-20.
+//
+
+import SwiftUI
+
+struct MainContainerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainContainerView()
+}
