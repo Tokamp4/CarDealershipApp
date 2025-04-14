f01d1caeba40b728c3143712170f6a68198a5397
arshathwuvais <wuvais@gmail.com>
2025-04-14 03:23:44 -0400

    Listingview when you press the listing view button the image repalces where the button was and hides the button and whenn you press the image again it brings the button back

---
+++ CarDealershipApp/ImagePicker.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePicker.swift
+//  CarDealershipApp
+//
+//  Created by Nizmee Wuvais on 2025-04-14.
+//
+
+import SwiftUI
+
+struct ImagePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImagePicker()
+}
