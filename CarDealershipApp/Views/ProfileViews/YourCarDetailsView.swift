6dffae9d883300471b22c7c2263165f107e0f3e5
Adrylo <adrilverine@gmail.com>
2025-04-15 00:28:00 -0400

    fixed your car section in profile view. made sold button so car gets deleted when user confirms this action.

---
+++ CarDealershipApp/Views/ProfileViews/YourCarDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  YourCarDetailsView.swift
+//  CarDealershipApp
+//
+//  Created by Adril Kemyem on 2025-04-14.
+//
+
+import SwiftUI
+
+struct YourCarDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    YourCarDetailsView()
+}
