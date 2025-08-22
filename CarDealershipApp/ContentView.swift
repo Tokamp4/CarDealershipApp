04fe8d16c6e3f160333ac229740e0b27866ed724
Tokamp4 <eduardo.tokarski.almeida@gmail.com>
2025-08-22 11:19:07 -0400

    test

--- CarDealershipApp/ContentView.swift
+++ CarDealershipApp/ContentView.swift
@@ -19,6 +19,7 @@ struct ContentView: View {
             } else {
                 NavigationView {
                     AuthMenuView()
+                    var m = 1
                 }
             }
         }
